42272271889c15294d2fce3bf6a9ba22da44e391
puria-asr <poriaasrzad@gmail.com>
2023-02-26 15:48:26 +0330

    first c++ commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World \n";
+      cout<<"Hello World ";
+    int simpleNumber =10 ;
+   bool simpleBool =false;
+    string simpleString ="simpleString";
+    double simpledouble=18.15;
+    char myLetter;
+    double sum =simpleNumber+simpledouble;
+cout <<simpleNumber <<"\n";
+cout <<simpledouble <<"\n";
+cout <<simpleString <<"\n";
+cout <<myLetter <<"\n";
+cout <<sum;
+
+    return 0;
+}
