85f55e14633098ade8b7e5fde1e6725bd217b494
puria-asr <poriaasrzad@gmail.com>
2023-02-26 22:40:03 +0330

    string added

---
+++ string.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // string firstName ="Pouria";
+    // string lastName ="Asrzad";
+    // string fullName =firstName.append(' '+lastName);
+    string addText;
+
+    cout << "tell a name we will tell you the letter: \n";
+
+    cin >> addText;
+
+    cout << addText.length();
+
+    return 0;
+}
