6d7c224c59fe8f87fc8d5adf051e7500a07084a9
puria-asr <poriaasrzad@gmail.com>
2023-03-08 19:52:58 +0330

    square app added

--- string.cpp
+++ string.cpp
@@ -1,21 +1,23 @@
 
 #include <iostream>
 #include <string>
-
+#include <cmath>
 using namespace std;
 
-int main()
-{
-    // string firstName ="Pouria";
-    // string lastName ="Asrzad";
-    // string fullName =firstName.append(' '+lastName);
-    string addText;
+int main() {
+	
+//	 string firstName ="Pouria";
+//	// string lastName ="Asrzad";
+//	// string fullName =firstName.append(' '+lastName);
+//	string addText;
+//	
+//	cout << "tell a name we will tell you the letter: \n";
+//
+//	cin >> addText;
+//	
+	cout << sqrt(64)<<"\n";
+	cout << round(64.15)<<"\n";
+	cout << log(2) <<"\n" ;	
 
-    cout << "tell a name we will tell you the letter: \n";
-
-    cin >> addText;
-
-    cout << addText.length();
-
-    return 0;
+	return 0;
 }
